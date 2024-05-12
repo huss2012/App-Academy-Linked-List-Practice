c2c949a265a13704a38321fa0c580d693fc71173
huss2012 <huss24940@gmail.com>
2024-05-12 10:43:52 +0300

    Doubly Linked List test specs have be written.

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -7,16 +7,37 @@ class DoublyLinkedListNode {
 }
 
 class DoublyLinkedList {
-  constructor() {
+  constructor(head = null, tail = null, length = 0) {
     // Your code here
+    this.head = head;
+    this.tail = tail;
+    this.length = length;
   }
 
   addToHead(val) {
     // Your code here
+    if (this.head) {
+      this.head.prev = new DoublyLinkedListNode(val);
+      this.head.prev.next = this.head;
+      this.head = this.head.prev;
+    } else {
+      this.head = new DoublyLinkedListNode(val);
+      this.tail = this.head;
+    }
+    this.length++;
   }
 
   addToTail(val) {
     // Your code here
+    if (this.tail) {
+      this.tail.next = new DoublyLinkedListNode(val);
+      this.tail.next.prev = this.tail;
+      this.tail = this.tail.next;
+    } else {
+      this.tail = new DoublyLinkedListNode(val);
+      this.head = this.tail;
+    }
+    this.length++;
   }
 
   // You can use this function to help debug
