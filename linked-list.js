ca6f9fa7e391f92b2ef56e74678604ab07476258
huss2012 <huss24940@gmail.com>
2024-05-12 07:28:08 +0300

    LinkedList test specs are solved.

--- linked-list.js
+++ linked-list.js
@@ -6,16 +6,39 @@ class LinkedListNode {
 }
 
 class LinkedList {
-  constructor() {
+  constructor(head = null, length = 0) {
     // Your code here
+    this.head = head;
+    this.length = length;
   }
 
   addToHead(val) {
     // Your code here
+    let newNode = new LinkedListNode(val);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
   }
 
   addToTail(val) {
     // Your code here
+    let newNode = new LinkedListNode(val);
+    //in case the list is empty.
+    if (this.head === null) {
+      this.head = newNode;
+      this.length++;
+      return;
+    }
+    let current = this.head;
+
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+
+    this.length++;
   }
 
   // You can use this function to help debug
